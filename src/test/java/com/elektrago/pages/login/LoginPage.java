5049db3a3a317d714f737acd19a9c0cbc1126d7e
danielfabfraterio <166540977+danielfabfraterio@users.noreply.github.com>
2024-12-27 10:22:13 -0600

    android elements for Login flows

--- src/test/java/com/elektrago/pages/login/LoginPage.java
+++ src/test/java/com/elektrago/pages/login/LoginPage.java
@@ -13,33 +13,43 @@ import org.openqa.selenium.WebElement;
 
 public class LoginPage {
 
+    @AndroidFindBy(id = "com.elektrago.elektrago:id/input_email")
     @iOSXCUITFindBy(accessibility = "usernameTextfield")
     public WebElement inputEmail;
 
+    @AndroidFindBy(id = "com.elektrago.elektrago:id/switch_option_text")
     @iOSXCUITFindBy(accessibility = "Use mobile number instead")
     public WebElement btnNumberInstead;
 
+    @AndroidFindBy(id = "com.elektrago.elektrago:id/input_password")
     @iOSXCUITFindBy(className = "XCUIElementTypeSecureTextField")
     public WebElement inputPasswd;
 
+    @AndroidFindBy(id = "com.elektrago.elektrago:id/text_forgot_password")
     @iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeStaticText[`name == \"Forgot password\"`]")
     public WebElement btnForgotPasswd;
 
+    @AndroidFindBy(id = "com.elektrago.elektrago:id/relativeLayout")
     @iOSXCUITFindBy(accessibility = "arrow down icon")
     public WebElement dropDownMobileCode;
 
+    @AndroidFindBy(xpath = "//android.widget.TextView[@resource-id=\"com.elektrago.elektrago:id/tvLada\" and @text=\"+52\"]")
     @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[contains(@name,\"+52\")]")
     public WebElement mexicanCodeNumber;
 
+    @AndroidFindBy(xpath = "//android.widget.TextView[@resource-id=\"com.elektrago.elektrago:id/tvLada\" and @text=\"+1\"]")
     @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[contains(@name,\"+1\")]")
     public WebElement americanCodeNumber;
 
+    @AndroidFindBy(id = "com.elektrago.elektrago:id/input_phone")
     @iOSXCUITFindBy(accessibility = "usernameTextfield")
     public WebElement inputMobileNumber;
 
+    @AndroidFindBy(id = "com.elektrago.elektrago:id/textError")
     @iOSXCUITFindBy(xpath = "//XCUIElementTypeImage[@name=\"info_red_Icon\"]/following-sibling::XCUIElementTypeStaticText")
     public WebElement msgIncorrectuserNameOrPassENG;
 
+    @AndroidFindBy(id = "com.elektrago.elektrago:id/button_continue")
     @iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeStaticText[`name == \"Login\"`]")
     public WebElement btnLogin;
 
