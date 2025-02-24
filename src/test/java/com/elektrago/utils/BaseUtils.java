350a34fd8d32d477224e44f1b401ee56c4e08d91
danielfabfraterio <166540977+danielfabfraterio@users.noreply.github.com>
2025-02-24 09:40:21 -0600

    merged to development

--- src/test/java/com/elektrago/utils/BaseUtils.java
+++ src/test/java/com/elektrago/utils/BaseUtils.java
@@ -19,13 +19,11 @@ public class BaseUtils {
     public static void waitUntilElementisPresent(WebElement element) {
         if (explicitWait == null) {
             explicitWait = new WebDriverWait(getDriver(), Duration.ofSeconds(SECONDS));
-            explicitWait = new WebDriverWait(getDriver(), Duration.ofSeconds(SECONDS));
         }
         explicitWait.until(ExpectedConditions.visibilityOf(element));
     }
 
     public static void fillUpField(WebElement element, String value) {
-        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         element.sendKeys(value);
     }
@@ -38,14 +36,12 @@ public class BaseUtils {
     public static void waitUntilElementIsClickable(WebElement element) {
         if (explicitWait == null) {
             explicitWait = new WebDriverWait(getDriver(), Duration.ofSeconds(SECONDS));
-            explicitWait = new WebDriverWait(getDriver(), Duration.ofSeconds(SECONDS));
         }
         explicitWait.until(ExpectedConditions.elementToBeClickable(element));
     }
 
     public static void waitUntilElementIsSelected(WebElement element) {
         if (explicitWait == null) {
-            explicitWait = new WebDriverWait(getDriver(), Duration.ofSeconds(SECONDS));
             explicitWait = new WebDriverWait(getDriver(), Duration.ofSeconds(SECONDS));
         }
         explicitWait.until(ExpectedConditions.elementToBeSelected(element));
